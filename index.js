19b46b92689962d3167878f21fb2bfa1748baf7d
GabrielSKlauck <112292289+GabrielSKlauck@users.noreply.github.com>
2024-03-21 17:45:14 -0300

    javascript feito

---
+++ index.js
@@ -0,0 +1,11 @@
+function showTerms(){
+    event.preventDefault();
+    var modal = document.getElementById('modal-termos');
+    modal.style.display = "flex"
+}
+
+function removeTerms(){
+    event.preventDefault();
+    var modal = document.getElementById('modal-termos');
+    modal.style.display = "none" 
+}
